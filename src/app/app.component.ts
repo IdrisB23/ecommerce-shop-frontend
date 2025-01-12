b180ab0e7c2f117abf0df34b07ae1f42b5c935a5
Idris Babay <idrissbabay@gmail.com>
2025-01-13 00:45:36 +0100

    add custom components to main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './features/product/components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce-shop-frontend';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
