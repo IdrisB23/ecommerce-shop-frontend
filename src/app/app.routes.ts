1baa84323973ec91c12e1c776563e406414d4f89
Idris Babay <idrissbabay@gmail.com>
2025-01-13 23:28:30 +0100

    add routing to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './features/product/components/product-list/product-list.component';
+import { PageNotFoundComponent } from './core/component/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: 'products', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
